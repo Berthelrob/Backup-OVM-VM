895bf43381e4e94d93289d8aa81da7dd329e3035
Robson Berthelsen <robson.brs@gmail.com>
2023-08-24 15:45:21 -0300

    Create coleta_erros_log_messages.sh
    Script para monitorar o arquivo /var/log/messages de SO's linux utilizando zabbix_sender

---
+++ coleta_erros_log_messages.sh
@@ -0,0 +1,34 @@
+#!/bin/bash
+
+# Script: coleta_messages.sh
+# Desenvolvido por: Robson Berthelsen
+# Data: 24/08/2023
+# Versão: 1
+
+# Descrição: Este script coleta mensagens de erro no arquivo de log /var/log/messages
+# e envia as mensagens para o Zabbix usando o utilitário zabbix_sender.
+
+# Dependências: Necessário ter o pacote zabbix_sender instalado
+
+# Configurações
+log_file="/var/log/messages"
+zabbix_server="10.150.25.56"  # Endereço IP do servidor Zabbix
+hostname=$(hostname -s)        # Nome do host
+zabbix_key="/Linux\ by\ Zabbix\ agent/message_log_errors"  # Chave do item no Zabbix
+
+# Padrões de erros a serem ignorados (separados por |)
+# Exemplo: ignored_errors="pattern1|pattern2|pattern3"
+ignored_errors="airflow: **kwargs: {'failed_states': 'failed'}"
+
+# Coleta de erros no log
+errors=$(tail -n 100 "$log_file" | grep -E -i "error|failed|miss" | grep -E -v "$ignored_errors")
+
+if [ -n "$errors" ]; then
+    while IFS= read -r line; do
+        # Envia cada mensagem de erro para o Zabbix
+        echo "$line" | zabbix_sender -z "$zabbix_server" -s "$hostname" -k "$zabbix_key" -o "$line"
+    done <<< "$errors"
+else
+    # Se não houver erros, envia "null" para o Zabbix
+    echo "" | zabbix_sender -z "$zabbix_server" -s "$hostname" -k "$zabbix_key" -o ""
+fi
